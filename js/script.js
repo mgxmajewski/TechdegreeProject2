559d658531e4e5ac43551963593519525bd72e44
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-03-01 13:55:31 +0100

    added appendPageLinks function
    also added paginationNumber and paginationDiv variables

--- js/script.js
+++ js/script.js
@@ -73,4 +73,27 @@ showPage(studentList, 1);
    functionality to the pagination buttons.
 ***/
 
+ const appendPageLinks = (list) => {
+   let paginationNumber = Math.ceil(list / studentsPerPage);
+   let paginationDiv = document.createElement(div);
+   paginationDiv.className = 'pagination';
+   
+   /*
+ 1. Determine how many pages are needed for the list by dividing the
+ total number of list items by the max number of items per page
+
+ 2. Create a div, give it the “pagination” class, and append it to the .page div
+ https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+
+ 3. Add a ul to the “pagination” div to store the pagination links
+ 4. for every page, add li and a tags with the page number text
+ 5. Add an event listener to each a tag. When they are clicked
+ call the showPage function to display the appropriate page
+6. Loop over pagination links to remove active class from all links
+7. Add the active class to the link that was just clicked. You can identify that
+clicked link using event.target
+*/
+}
+
+
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
